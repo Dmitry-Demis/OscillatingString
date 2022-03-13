4521d2a95fc7b6b557f4e5a09b33697f44429a59
Dmitry-Dzyuba <diman.secret@mail.ru>
2022-03-13 23:40:28 +0300

    Steps for drawing

--- OscillatingString/MainWindow.xaml.cs
+++ OscillatingString/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OscillatingString
 {
@@ -42,9 +36,23 @@ namespace OscillatingString
 
         private long step,
             err;
+
+        private int Wxl,
+            Wyl,
+            Wxr,
+            Wyr,
+            Wyc,
+            bx,
+            dx,
+            rx;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(()=>Go());
         }
 
         private void VarInit()
@@ -79,6 +87,76 @@ namespace OscillatingString
             deltaG[4] = dG * sn * e;
             for (var i = 0; i < 4; i++) G[i] += deltaG[i];
             err++;
+        }
+
+        private void NewStep()
+        {
+            for (var i = 1; i < N-1; i++) f[i] = U[i + 1] - 2 * U[i] + U[i - 1];
+            for (var i = 1; i < N - 1; i++) Un[i] = 2 * U[i] - Us[i] + r * f[i];
+            for (var i = 0; i < N; i++) Ut[i] = 0;
+            for (var j = 1; j < N-1; j++)
+            {
+                for (var i = -1; i < 1; i++)
+                {
+                    Ut[j] += G[2 + i] * U[j + i];
+                    Ut[j] += G[4] * Us[j];
+                }
+            }
+            var dU = Ut[Nt] - Un[Nt];
+            if (dU < -dH)  Teach(1);
+            if (dU > dH) Teach(-1);
+            step++;
+        }
+        private static BitmapImage BmpImageFromBmp(Bitmap bmp)
+        {
+            if (bmp == null) throw new ArgumentNullException(nameof(bmp));
+            using (var memory = new System.IO.MemoryStream())
+            {
+                bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+                return bitmapImage;
+            }
+        }
+        private void Render()
+        {
+            using (var bmp = new Bitmap((int)MyCanvas.ActualWidth, (int)MyCanvas.ActualHeight))
+            using (var gfx = Graphics.FromImage(bmp))
+            using (var pen = new Pen(Color.Aqua))
+            {
+                // draw one thousand random white lines on a dark blue background
+                gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                gfx.Clear(Color.FromArgb(30, 30, 30));
+                var pen2 = new Pen(Color.Bisque);
+                this.Dispatcher.Invoke(() =>
+                {
+                    gfx?.DrawLine(pen, 0, (int)(MyCanvas.ActualHeight / 2), (int)MyCanvas.ActualWidth, (int)(MyCanvas.ActualHeight / 2));
+                    gfx?.DrawLine(pen, (int)(MyCanvas.ActualWidth / 2), 0, (int)(MyCanvas.ActualWidth / 2), (int)(MyCanvas.ActualHeight));
+                    Random random = new Random();
+                    for (int i = 0; i < G.Length-1; i++)
+                    {
+                        gfx?.DrawLine(pen2, (int) G[i], random.Next(1, (int)MyCanvas.ActualHeight), (int)G[i+1], random.Next(1, (int)MyCanvas.ActualHeight));
+                    }
+                });
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    MyImage.Source = BmpImageFromBmp(bmp);
+                });
+            }
+        }
+
+        private void Go()
+        {
+            while (true)
+            {
+                Render();
+            }
         }
         private void MyCanvas_OnLoaded(object sender, RoutedEventArgs e)
         {
