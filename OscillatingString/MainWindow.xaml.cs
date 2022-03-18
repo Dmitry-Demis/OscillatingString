4651cae5048fa34b51627a0206d7e47976afb15b
Dmitry-Dzyuba <diman.secret@mail.ru>
2022-03-18 11:00:55 +0300

    Solved with pause

--- OscillatingString/MainWindow.xaml.cs
+++ OscillatingString/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using System.Threading;
 
 namespace OscillatingString
 {
@@ -37,6 +38,7 @@ namespace OscillatingString
         private double[] G = new double[4];
         private int Nt;
         private double r;
+        private double[] X;
 
         private long step,
             err;
@@ -54,7 +56,7 @@ namespace OscillatingString
 
         private int CanvasHeight;
         private int CanvasWidth;
-
+        private volatile bool pause = false;
 
 
         public MainWindow()
@@ -65,6 +67,7 @@ namespace OscillatingString
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            
             Task.Run(Go);
         }
 
@@ -87,33 +90,48 @@ namespace OscillatingString
             err = 0;
         }
 
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            pause = true;
+            StartButton.Visibility = Visibility.Visible;
+            PauseButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            pause = false;
+            StartButton.Visibility = Visibility.Collapsed;
+            PauseButton.Visibility = Visibility.Visible;
+        }
+
         private void Teach(double e)
         {
             var deltaG = new double[4];
             int sn;
-            for (var i = -1; i < 1; i++)
+            for (var i = -1; i < 2; i++)
             {
                 sn = U[Nt + i] < 0 ? -1 : 1;
-                deltaG[2 + i] = dG * sn * e;
+                deltaG[1 + i] = dG * sn * e;
             }
-            sn = U[Nt] < 0 ? -1 : 1;
-            deltaG[4] = dG * sn * e;
-            for (var i = 0; i < 4; i++) G[i] += deltaG[i];
+            sn = Us[Nt] < 0 ? -1 : 1;
+            deltaG[3] = dG * sn * e;
+            for (var i = 0; i < G.Length; i++) G[i] += deltaG[i];
             err++;
         }
 
         private void NewStep()
         {
-            for (var i = 1; i < N - 1; i++) f[i] = U[i + 1] - 2 * U[i] + U[i - 1];
+            for (var i = 1; i < N - 1; i++) f[i] = U[i + 1] - 2.0 * U[i] + U[i - 1];
             for (var i = 1; i < N - 1; i++) Un[i] = 2 * U[i] - Us[i] + r * f[i];
             for (var i = 0; i < N; i++) Ut[i] = 0;
             for (var j = 1; j < N - 1; j++)
             {
-                for (var i = -1; i < 1; i++)
+                for (var i = -1; i <= 1; i++)
                 {
-                    Ut[j] += G[2 + i] * U[j + i];
-                    Ut[j] += G[4] * Us[j];
+                    Ut[j] += G[1 + i] * U[j + i];                   
                 }
+                Ut[j] += G[3] * Us[j];
             }
             var dU = Ut[Nt] - Un[Nt];
             if (dU < -dH) Teach(1);
@@ -139,42 +157,63 @@ namespace OscillatingString
         }
         private void Render()
         {
-            using (var bmp = new Bitmap((int)MyCanvas.ActualWidth, (int)MyCanvas.ActualHeight))
+            using (var bmp = new Bitmap((int)CanvasWidth, (int)CanvasHeight))
             using (var gfx = Graphics.FromImage(bmp))
             using (var pen = new Pen(Color.Aqua))
             {
                 // draw one thousand random white lines on a dark blue background
                 gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
                 gfx.Clear(Color.FromArgb(30, 30, 30));
-                var pen2 = new Pen(Color.Bisque);
-                this.Dispatcher.Invoke(() =>
+                var pen2 = new Pen(Color.Red, 3);
+                var pen3 = new Pen(Color.Yellow, 3);
+                Dispatcher.Invoke(() =>
                 {
-                    gfx?.DrawLine(pen, 0, (int)(MyCanvas.ActualHeight / 2), (int)MyCanvas.ActualWidth, (int)(MyCanvas.ActualHeight / 2));
-                    gfx?.DrawLine(pen, (int)(MyCanvas.ActualWidth / 2), 0, (int)(MyCanvas.ActualWidth / 2), (int)(MyCanvas.ActualHeight));
+                    gfx?.DrawLine(pen, 0, (int)(CanvasHeight / 2), (int)CanvasWidth, (int)(CanvasHeight / 2));
+                    gfx?.DrawLine(pen, (int)(CanvasWidth / 2), 0, (int)(CanvasWidth / 2), (int)(CanvasHeight));
                     Random random = new Random();
-                    for (int i = 0; i < G.Length - 1; i++)
+                    for (int i = 0; i < N - 1; i++)
                     {
-                        gfx?.DrawLine(pen2, (int)G[i], random.Next(1, (int)MyCanvas.ActualHeight), (int)G[i + 1], random.Next(1, (int)MyCanvas.ActualHeight));
+                        gfx?.DrawLine(pen2, (float)X[i], (float)(CanvasHeight /2 + mtt * U[i]), (float)X[i+1], (float)(CanvasHeight /2+ mtt * U[i+1]));
+                        gfx?.DrawLine(pen3, (float)X[i], (float)(CanvasHeight / 2 + mtt * Ut[i]), (float)X[i + 1], (float)(CanvasHeight / 2 + mtt * Ut[i + 1]));
                     }
                 });
 
-                this.Dispatcher.Invoke(() =>
+                Dispatcher.Invoke(() =>
                 {
                     MyImage.Source = BmpImageFromBmp(bmp);
                 });
             }
         }
-
+        ManualResetEvent _pauseEvent = new ManualResetEvent(true);
         private void Go()
         {
+            VarInit();           
             while (true)
             {
-                this.Dispatcher.Invoke(() =>
+
+                while (pause)
+                {
+
+                }
+                
+                NewStep();
+                for (int i = 0; i < N; i++)
+                {
+                    Us[i]= U[i];
+                    U[i]= Un[i];
+                }
+                Dispatcher.Invoke(() =>
                 {
                     StepTextBox.Text = step.ToString();
+                    G1TextBox.Text = G[0].ToString();
+                    G2TextBox.Text = G[1].ToString();
+                    G3TextBox.Text = G[2].ToString();
+                    G4TextBox.Text = G[3].ToString();
                 });
                 step++;
                 Render();
+              //  Thread.Sleep(10);
+             //   Render();
             }
         }
         private void MyCanvas_OnLoaded(object sender, RoutedEventArgs e)
@@ -189,6 +228,17 @@ namespace OscillatingString
             }
             CanvasHeight = (int)(dpiY * MyCanvas.ActualHeight);
             CanvasWidth = (int)(dpiX * MyCanvas.ActualWidth);
+            int countOfSegments = CanvasWidth / N;
+            X = new double[N];
+            for (int i = 0; i < X.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    X[i] += countOfSegments;
+                }
+            }
+            CanvasWidth = N * countOfSegments;
+
         }
 
         private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
@@ -197,27 +247,27 @@ namespace OscillatingString
         }
         private void dGTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+   
         }
 
         private void G1TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            G1TextBox.Text = G[0].ToString();
         }
 
         private void G2TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            G2TextBox.Text = G[1].ToString();
         }
 
         private void G3TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            G3TextBox.Text = G[2].ToString();
         }
 
         private void G4TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            G4TextBox.Text = G[3].ToString();
         }
 
         private void dHTextBox_TextChanged(object sender, TextChangedEventArgs e)
